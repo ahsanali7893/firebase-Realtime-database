4496d54e019b3fff206a0a629c37e18ea22e71fd
ahsanali7893 <ahsan3916902@gamil.com>
2024-02-27 13:30:20 +0500

    initial change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <div className="max-w-6xl mx-auto pt-52">
       <h1 className="text-2xl font-bold text-center pb-9">fetch data from Realtime DB</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {users.map((users: any) => (
           <div key={users.id} className="bg-gray-100 p-4 rounded-lg">
             <h2 className="text-2xl text-gray-900">{users.title}</h2>
